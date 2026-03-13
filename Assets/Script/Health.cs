fe842e9edf5df0394b445dd0fff1fcd9dba8ee85
Trang1711 <146830798+Trang1711@users.noreply.github.com>
2026-03-13 17:55:46 +0700

    demo7

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -2,31 +2,39 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
-	public GameObject explosionPrefab;
-	public int defaultHealthPoint = 1;
-	private int healthPoint;
-	public System.Action onDead;
-	private void Start()
-	{
-		healthPoint = defaultHealthPoint;
-	}
+    public GameObject explosionPrefab; 
+    public int defaultHealthPoint = 3; 
+    public int healthPoint;     
 
-	public void TakeDamage(int damage)
-	{
-		if (healthPoint <= 0) return;
+    public System.Action onHealthChanged;    
+    
+    public System.Action onDead;
+    private void Start()
+    {
+        healthPoint = defaultHealthPoint; 
+        onHealthChanged?.Invoke();
+    }
 
-		healthPoint -= damage;
-		if (healthPoint <= 0) Die();
-	}
+    public void TakeDamage(int damage)
+    {
+        if (healthPoint <= 0) return; 
 
-	protected virtual void Die()
-	{
-		if (explosionPrefab != null)
-		{
-			var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
-			Destroy(explosion, 1f);
-		}
-		Destroy(gameObject);
-		onDead?.Invoke();
-	}
+        healthPoint -= damage;
+        onHealthChanged?.Invoke();
+        if (healthPoint <= 0)
+        {
+            Die();
+        }
+    }
+
+    protected virtual void Die()
+    {
+        if (explosionPrefab != null)
+        {
+            var explosion = Instantiate(explosionPrefab, transform.position, transform.rotation);
+            Destroy(explosion, 1f);
+        }
+        Destroy(gameObject);
+        onDead?.Invoke();
+    }
 }
